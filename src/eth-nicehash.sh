b85d599368ee93818785e699c34186e85b667a29
Jerry Nguyen <jerry.nguyen@cylo.ai>
2024-02-03 19:24:19 +0700

    Update

--- src/eth-nicehash.sh
+++ src/eth-nicehash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./t-rex -a ethash -o stratum+ssl://sha256.auto.nicehash.com:9200 -u NHbTcEswNPjtNgpwqZD2tWYJ3KZq5Gd9NHV9 --api-bind-http 0.0.0.0:4067
+./t-rex -a ethash -o stratum+ssl://sha256.auto.nicehash.com:443 -u NHbTcEswNPjtNgpwqZD2tWYJ3KZq5Gd9NHV9 --api-bind-http 0.0.0.0:4067
